2ad02f642c2dcbbdadb0f23b02884d838b9ea600
adityaRaj712 <hiitsaditya7369@gmail.com>
2024-02-03 10:40:16 +0530

    routes

--- ecomm/src/components/pages/Home/Home.jsx
+++ ecomm/src/components/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MainComponent from '../../main/maincomponent'
 import Navbar from '../../navbar/navigation'
-import Footer from '../../footer/footer'
+import Footer from '../../footer/Footer'
 
 const Home = () => {
   console.log(`rendering`);
